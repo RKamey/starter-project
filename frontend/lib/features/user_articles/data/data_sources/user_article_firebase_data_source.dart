709966d5f85bacc34686f9c034c316dad526cbcd
alonsokamey <alonsokamey@gmail.com>
2026-02-02 16:22:41 -0600

    feat(user-articles): add Firebase data source for user articles

---
+++ frontend/lib/features/user_articles/data/data_sources/user_article_firebase_data_source.dart
@@ -0,0 +1,86 @@
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:news_app_clean_architecture/features/user_articles/data/models/user_article_model.dart';
+
+abstract class UserArticleFirebaseDataSource {
+  Future<List<UserArticleModel>> getUserArticles();
+  Future<void> createArticle(UserArticleModel article);
+  Future<void> updateArticle(UserArticleModel article);
+  Future<void> deleteArticle(String articleId);
+  Future<String> uploadThumbnail(File imageFile, String articleId);
+}
+
+class UserArticleFirebaseDataSourceImpl implements UserArticleFirebaseDataSource {
+  final FirebaseFirestore _firestore;
+  final FirebaseStorage _storage;
+
+  UserArticleFirebaseDataSourceImpl(this._firestore, this._storage);
+
+  @override
+  Future<List<UserArticleModel>> getUserArticles() async {
+    try {
+      final querySnapshot = await _firestore
+          .collection('articles')
+          .orderBy('createdAt', descending: true)
+          .get();
+
+      return querySnapshot.docs
+          .map((doc) => UserArticleModel.fromFirestore(doc))
+          .toList();
+    } catch (e) {
+      throw Exception('Failed to get articles: $e');
+    }
+  }
+
+  @override
+  Future<void> createArticle(UserArticleModel article) async {
+    try {
+      final docRef = _firestore.collection('articles').doc(article.id);
+      await docRef.set(article.toFirestore());
+    } catch (e) {
+      throw Exception('Failed to create article: $e');
+    }
+  }
+
+  @override
+  Future<void> updateArticle(UserArticleModel article) async {
+    try {
+      await _firestore
+          .collection('articles')
+          .doc(article.id)
+          .update(article.toFirestore());
+    } catch (e) {
+      throw Exception('Failed to update article: $e');
+    }
+  }
+
+  @override
+  Future<void> deleteArticle(String articleId) async {
+    try {
+      // Delete from Firestore
+      await _firestore.collection('articles').doc(articleId).delete();
+
+      // Delete thumbnail from Storage
+      try {
+        await _storage.ref('media/articles/$articleId').delete();
+      } catch (e) {
+        // If thumbnail doesn't exist, continue
+      }
+    } catch (e) {
+      throw Exception('Failed to delete article: $e');
+    }
+  }
+
+  @override
+  Future<String> uploadThumbnail(File imageFile, String articleId) async {
+    try {
+      final storageRef = _storage.ref('media/articles/$articleId');
+      await storageRef.putFile(imageFile);
+      final downloadURL = await storageRef.getDownloadURL();
+      return downloadURL;
+    } catch (e) {
+      throw Exception('Failed to upload thumbnail: $e');
+    }
+  }
+}
