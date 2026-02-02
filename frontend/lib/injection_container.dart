fb4aee90f8040d248403dfc4ab4698c422fefea3
alonsokamey <alonsokamey@gmail.com>
2026-02-02 17:53:09 -0600

    feat(user-articles): add Firebase dependencies and user article use cases to the injection container

--- frontend/lib/injection_container.dart
+++ frontend/lib/injection_container.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:get_it/get_it.dart';
 import 'package:dio/dio.dart';
 import 'package:news_app_clean_architecture/features/daily_news/data/data_sources/remote/news_api_service.dart';
@@ -5,6 +7,14 @@ import 'package:news_app_clean_architecture/features/daily_news/data/repository/
 import 'package:news_app_clean_architecture/features/daily_news/domain/repository/article_repository.dart';
 import 'package:news_app_clean_architecture/features/daily_news/domain/usecases/get_article.dart';
 import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
+import 'package:news_app_clean_architecture/features/user_articles/data/data_sources/user_article_firebase_data_source.dart';
+import 'package:news_app_clean_architecture/features/user_articles/data/repositories/user_article_repository_impl.dart';
+import 'package:news_app_clean_architecture/features/user_articles/domain/repositories/user_article_repository.dart';
+import 'package:news_app_clean_architecture/features/user_articles/domain/usecases/create_user_articles.dart';
+import 'package:news_app_clean_architecture/features/user_articles/domain/usecases/delete_user_articles.dart';
+import 'package:news_app_clean_architecture/features/user_articles/domain/usecases/get_user_articles.dart';
+import 'package:news_app_clean_architecture/features/user_articles/domain/usecases/upload_thumbnail.dart';
+import 'package:news_app_clean_architecture/features/user_articles/presentation/bloc/user_article_bloc.dart';
 import 'features/daily_news/data/data_sources/local/app_database.dart';
 import 'features/daily_news/domain/usecases/get_saved_article.dart';
 import 'features/daily_news/domain/usecases/remove_article.dart';
@@ -21,14 +31,27 @@ Future<void> initializeDependencies() async {
   // Dio
   sl.registerSingleton<Dio>(Dio());
 
-  // Dependencies
+  // Firebase
+  sl.registerSingleton<FirebaseFirestore>(FirebaseFirestore.instance);
+  sl.registerSingleton<FirebaseStorage>(FirebaseStorage.instance);
+
+  // Dependencies - Daily News
   sl.registerSingleton<NewsApiService>(NewsApiService(sl()));
 
   sl.registerSingleton<ArticleRepository>(
     ArticleRepositoryImpl(sl(),sl())
   );
+
+  // Dependencies - User Articles
+  sl.registerSingleton<UserArticleFirebaseDataSource>(
+    UserArticleFirebaseDataSourceImpl(sl(), sl())
+  );
+
+  sl.registerSingleton<UserArticleRepository>(
+    UserArticleRepositoryImpl(sl())
+  );
   
-  //UseCases
+  //UseCases - Daily News
   sl.registerSingleton<GetArticleUseCase>(
     GetArticleUseCase(sl())
   );
@@ -45,8 +68,25 @@ Future<void> initializeDependencies() async {
     RemoveArticleUseCase(sl())
   );
 
+  // UseCases - User Articles
+  sl.registerSingleton<GetUserArticlesUseCase>(
+    GetUserArticlesUseCase(sl())
+  );
 
-  //Blocs
+  sl.registerSingleton<CreateUserArticleUseCase>(
+    CreateUserArticleUseCase(sl())
+  );
+
+  sl.registerSingleton<DeleteUserArticleUseCase>(
+    DeleteUserArticleUseCase(sl())
+  );
+
+  sl.registerSingleton<UploadThumbnailUseCase>(
+    UploadThumbnailUseCase(sl())
+  );
+
+
+  //Blocs - Daily News
   sl.registerFactory<RemoteArticlesBloc>(
     ()=> RemoteArticlesBloc(sl())
   );
@@ -55,5 +95,8 @@ Future<void> initializeDependencies() async {
     ()=> LocalArticleBloc(sl(),sl(),sl())
   );
 
-
+  // Blocs - User Articles
+  sl.registerFactory<UserArticlesBloc>(
+    ()=> UserArticlesBloc(sl(), sl(), sl(), sl())
+  );
 }
