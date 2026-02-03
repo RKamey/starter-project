e51d4c58fe2e61bf5a7dd91bb6f8120ce25c31ac
alonsokamey <alonsokamey@gmail.com>
2026-02-03 01:42:43 -0600

    feat(daily-news): convert DailyNews to StatefulWidget and implement article loading logic

--- frontend/lib/features/daily_news/presentation/pages/home/daily_news.dart
+++ frontend/lib/features/daily_news/presentation/pages/home/daily_news.dart
@@ -2,20 +2,32 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
+import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_event.dart';
 import 'package:news_app_clean_architecture/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart';
 
 import '../../../domain/entities/article.dart';
 import '../../widgets/article_tile.dart';
 
-class DailyNews extends StatelessWidget {
+class DailyNews extends StatefulWidget {
   const DailyNews({Key? key}) : super(key: key);
+
+  @override
+  State<DailyNews> createState() => _DailyNewsState();
+}
+
+class _DailyNewsState extends State<DailyNews> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<RemoteArticlesBloc>().add(const GetArticles());
+  }
 
   @override
   Widget build(BuildContext context) {
     return _buildPage();
   }
 
-  _buildAppbar(BuildContext context) {
+  AppBar _buildAppbar(BuildContext context) {
     return AppBar(
       title: const Text(
         'Daily News',
@@ -33,7 +45,7 @@ class DailyNews extends StatelessWidget {
     );
   }
 
-  _buildPage() {
+  Widget _buildPage() {
     return BlocBuilder<RemoteArticlesBloc, RemoteArticlesState>(
       builder: (context, state) {
         if (state is RemoteArticlesLoading) {
@@ -56,6 +68,34 @@ class DailyNews extends StatelessWidget {
 
   Widget _buildArticlesPage(
       BuildContext context, List<ArticleEntity> articles) {
+    if (articles.isEmpty) {
+      return Scaffold(
+        appBar: _buildAppbar(context),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.article_outlined, size: 64, color: Colors.grey),
+              SizedBox(height: 16),
+              Text(
+                'No articles yet',
+                style: TextStyle(fontSize: 18, color: Colors.grey),
+              ),
+              SizedBox(height: 8),
+              Text(
+                'Tap + to create your first article',
+                style: TextStyle(fontSize: 14, color: Colors.grey),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => _onCreateArticlePressed(context),
+          child: const Icon(Icons.add),
+        ),
+      );
+    }
+
     List<Widget> articleWidgets = [];
     for (var article in articles) {
       articleWidgets.add(ArticleWidget(
@@ -66,8 +106,14 @@ class DailyNews extends StatelessWidget {
 
     return Scaffold(
       appBar: _buildAppbar(context),
-      body: ListView(
-        children: articleWidgets,
+      body: RefreshIndicator(
+        onRefresh: () async {
+          context.read<RemoteArticlesBloc>().add(const GetArticles());
+          await Future.delayed(const Duration(milliseconds: 500));
+        },
+        child: ListView(
+          children: articleWidgets,
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => _onCreateArticlePressed(context),
@@ -84,7 +130,11 @@ class DailyNews extends StatelessWidget {
     Navigator.pushNamed(context, '/SavedArticles');
   }
 
-  void _onCreateArticlePressed(BuildContext context) {
-    Navigator.pushNamed(context, '/CreateArticle');
+  Future<void> _onCreateArticlePressed(BuildContext context) async {
+    await Navigator.pushNamed(context, '/CreateArticle');
+
+    if (mounted) {
+      context.read<RemoteArticlesBloc>().add(const GetArticles());
+    }
   }
 }
